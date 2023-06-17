39e33982bd8b8fa38da3da718d2b698cfa8bc41f
lixianx xing <lixian666@mail.ynu.edu.cn>
2023-06-17 05:23:01 +0000

    add splash

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,43 @@ import 'ToDoList.dart';
 import 'memo.dart';
 import 'game.dart';
 import 'change.dart';
+import 'splash.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tab Bar App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(), // 显示闪屏页面
+      routes: {
+        '/home': (context) => Home(), // 主页面的路由
+      },
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   int _selectedTabIndex = 0;
 
   List<Widget> _tabList = [
     MemoApp(),
-   // MemoSearchScreen(),
+    // MemoSearchScreen(),
     TodoListPage(),
     Calculator(),
     Game(),
-    Converter(),   
+    Converter(),
   ];
 
   void _onItemTapped(int index) {
@@ -53,9 +68,9 @@ class _MyAppState extends State<MyApp> {
               icon: Icon(Icons.note, size: 50, color: Colors.black),
               label: 'Memo_Notes',
             ),
-           // BottomNavigationBarItem(
-             // icon: Icon(Icons.search, size: 50, color: Colors.black),
-              //label: 'Search',
+            // BottomNavigationBarItem(
+            // icon: Icon(Icons.search, size: 50, color: Colors.black),
+            //label: 'Search',
             //),
             BottomNavigationBarItem(
               icon: Icon(Icons.done, size: 50, color: Colors.black),
@@ -66,11 +81,11 @@ class _MyAppState extends State<MyApp> {
               label: 'Calculator',
             ),
             BottomNavigationBarItem(
-              icon:Icon(Icons.gamepad, size: 50, color: Colors.black),
+              icon: Icon(Icons.gamepad, size: 50, color: Colors.black),
               label: 'Game',
             ),
-             BottomNavigationBarItem(
-              icon:Icon(Icons.change_circle, size: 50, color: Colors.black),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.change_circle, size: 50, color: Colors.black),
               label: 'Change',
             ),
           ],
---
+++ lib/splash.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'main.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  int _countdown = 5;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  void startTimer() {
+    const oneSec = Duration(seconds: 1);
+    _timer = Timer.periodic(oneSec, (timer) {
+      setState(() {
+        if (_countdown == 0) {
+          timer.cancel();
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Home()),
+          );
+        } else {
+          _countdown--;
+        }
+      });
+    });
+  }
+
+  void navigateToNotes() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => Home()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // 背景图片
+          Image.network(
+            'https://raw.githubusercontent.com/lixianx/my_NetFeach/main/%E9%A1%B5%E9%9D%A2%201%20(2).png',
+            fit: BoxFit.cover,
+          ),
+          // 右上角倒计时和跳过按钮
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  GestureDetector(
+                    onTap: navigateToNotes,
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.black54,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Text(
+                        '跳过 $_countdown',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
